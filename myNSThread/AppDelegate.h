fa67c09a52fa1d57fd00af7acd56b5b542ae3ca2
qianfeng <qianfeng@qianfengdeMacBook-Pro.local>
2014-05-20 19:19:58 +0800

    Initial Commit

---
+++ myNSThread/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  myNSThread
+//
+//  Created by qianfeng on 14-5-20.
+//  Copyright (c) 2014年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
